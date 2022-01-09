d2871e5b7d4e34d4b365ff8e8e895f8fdf2675f5
Jason Mohabir <jmohabir30@stuy.edu>
2022-01-09 17:05:40 -0500

    add randoms

--- Driver.java
+++ Driver.java
@@ -6,26 +6,29 @@ public class Driver{
 
 		ArrayList glen = Sorts.makeOrderedArrayList(size);
 		System.out.println( "Best Cases: ");
-
 		Sorts.bubble( glen );
 		System.out.println( "Bubble Swaps: " + Sorts.swapCounter);
-
 		Sorts.selection( glen );
 		System.out.println( "Selection Swaps: " + Sorts.swapCounter);
-
 		Sorts.insertion( glen );
 		System.out.println( "Insertion Swaps: " + Sorts.swapCounter + "\n");
 
+		// A random order of the numbers to comapre with the ascending and descending
+		Sorts.shuffle(glen);
+		System.out.println( "Random Cases: ");
+		Sorts.bubble( glen );
+		System.out.println( "Bubble Swaps: " + Sorts.swapCounter);
+		Sorts.selection( glen );
+		System.out.println( "Selection Swaps: " + Sorts.swapCounter);
+		Sorts.insertion( glen );
+		System.out.println( "Insertion Swaps: " + Sorts.swapCounter + "\n");
 
 		glen=Sorts.makeReverseArrayList(size);
 		System.out.println( "Worst Cases: ");
-
 		Sorts.bubble( glen );
 		System.out.println( "Bubble Swaps: " + Sorts.swapCounter);
-
 		Sorts.selection( glen );
 		System.out.println( "Selection Swaps: " + Sorts.swapCounter);
-
 		Sorts.insertion( glen );
 		System.out.println( "Insertion Swaps: " + Sorts.swapCounter + "\n");
 	}
