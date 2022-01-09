3ec7c791616ac968b7619d2d2ffec15adbf15174
Jason Mohabir <jmohabir30@stuy.edu>
2022-01-09 16:38:59 -0500

    streamline driver

--- Driver.java
+++ Driver.java
@@ -2,31 +2,31 @@ import java.util.ArrayList;
 
 public class Driver{
 	public static void main(String[] args){
-		ArrayList glen = Sorts.makeOrderedArrayList(5);
+		int size=5;
 
-		System.out.println( "ArrayList glen before sort:\n" +  glen + "\n");
+		ArrayList glen = Sorts.makeOrderedArrayList(size);
+		System.out.println( "Best Cases: ");
 
-		System.out.println( "ArrayList glen after bubble:\n" + Sorts.bubble( glen ) );
-		System.out.println( "Bubble Swaps: " + Sorts.swapCounter + "\n");
+		Sorts.bubble( glen );
+		System.out.println( "Bubble Swaps: " + Sorts.swapCounter);
 
-		System.out.println( "ArrayList glen after selection:\n" + Sorts.selection( glen ) );
-		System.out.println( "Selection Swaps: " + Sorts.swapCounter + "\n");
+		Sorts.selection( glen );
+		System.out.println( "Selection Swaps: " + Sorts.swapCounter);
 
-		System.out.println( "ArrayList glen after insertion:\n" + Sorts.insertion( glen ) );
+		Sorts.insertion( glen );
 		System.out.println( "Insertion Swaps: " + Sorts.swapCounter + "\n");
 
 
-		glen=Sorts.makeReverseArrayList(5);
+		glen=Sorts.makeReverseArrayList(size);
+		System.out.println( "Worst Cases: ");
 
-		System.out.println( "(Start)ArrayList glen before sort:\n" +  glen + "\n");
+		Sorts.bubble( glen );
+		System.out.println( "Bubble Swaps: " + Sorts.swapCounter);
 
-		System.out.println( "ArrayList glen after bubble:\n" + Sorts.bubble( glen ) );
-		System.out.println( "Bubble Swaps: " + Sorts.swapCounter + "\n");
+		Sorts.selection( glen );
+		System.out.println( "Selection Swaps: " + Sorts.swapCounter);
 
-		System.out.println( "ArrayList glen after selection:\n" + Sorts.selection( glen ) );
-		System.out.println( "Selection Swaps: " + Sorts.swapCounter + "\n");
-
-		System.out.println( "ArrayList glen after insertion:\n" + Sorts.insertion( glen ) );
+		Sorts.insertion( glen );
 		System.out.println( "Insertion Swaps: " + Sorts.swapCounter + "\n");
 	}
 }
